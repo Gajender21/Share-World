7d7eca0820678c8bd73814aab4393b68160df384
Gajender <singhgajender026@gmail.com>
2024-04-26 12:07:14 +0530

    new changes

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,7 +4,7 @@ import {
 import { createPost, createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getInfinitePosts, getPostById, getRecentPosts, getUserById, getUsers, likePost, savePost, searchPosts, signInAccount, signOutAccount, updateFollowings, updatePost, updateUser } from "../appwrite/api";
 import { INewPost, INewUser, IUpdatePost, IUpdateUser } from "@/types";
 import { QUERY_KEYS } from "./queryKeys";
-import { date } from "zod";
+
 
 export const useCreateUserAccount = () => {
   return useMutation({
@@ -181,14 +181,17 @@ export const useDeletePost = () => {
 export const useGetPosts = () => {
   return useInfiniteQuery({
     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-    queryFn: getInfinitePosts,
-    getNextPageParam: (lastPage) => {
-      if (lastPage && lastPage.documents.length === 0) return null;
-
-      const lastId = lastPage?.documents[lastPage.documents.length - 1].$id;
+    queryFn: getInfinitePosts as any,
+    getNextPageParam: (lastPage: any) => {
+      if (!lastPage || lastPage.documents.length === 0) {
+        return null;
+      }
+  
+      const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
       return lastId;
-    }
-  })
+    },
+    initialPageParam: null, // Set initialPageParam to null
+  });
 }
 export const useSearchPost = (searchTerm: string) => {
   return useQuery({
--- src/root/pages/Explore.tsx
+++ src/root/pages/Explore.tsx
@@ -31,9 +31,7 @@ const Explore = () => {
   }
 
   const shouldShowSearchResults = searchValue !== "";
-  const shouldShowPosts =
-    !shouldShowSearchResults &&
-    posts.pages.every((item) => item?.documents.length === 0);
+  const shouldShowPosts =!shouldShowSearchResults && posts.pages.every((item) => item?.documents.length === 0);
 
   return (
     <div className="explore-container">
@@ -69,15 +67,15 @@ const Explore = () => {
       </div>
       <div className="flex flex-wrap gap-9 w-full max-w-5xl">
         {shouldShowSearchResults ? (
-          <SearchResults
+         searchPosts && <SearchResults
             isSearchFetching={isSearchFetching}
-            searchedPosts={searchPosts}
+            searchedPosts={searchPosts.documents}
           />
         ) : shouldShowPosts ? (
           <p className="text-light-4 mt-10 text-center w-full">End of Posts</p>
         ) : (
           posts.pages.map((item, index) => (
-            <GridPostList key={`page-${index}`} posts={item?.documents} />
+           item && <GridPostList key={`page-${index}`} posts={item?.documents} />
           ))
         )}
       </div>
